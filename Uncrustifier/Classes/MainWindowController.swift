40636910b6c136532b263b0f520c392d1fd6d401
macmade <macmade@xs-labs.com>
2022-09-28 21:30:07 +0200

    Project import.

---
+++ Uncrustifier/Classes/MainWindowController.swift
@@ -0,0 +1,179 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Jean-David Gadina - www.xs-labs.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+import Cocoa
+
+public class MainWindowController: NSWindowController
+{
+    @IBOutlet private var configTextView: NSTextView!
+    @IBOutlet private var codeTextView:   NSTextView!
+    
+    @objc private dynamic var config = ""
+    @objc private dynamic var code   = ""
+    
+    private var cacheDirectory:  URL?
+    private var configCacheFile: URL?
+    private var codeCacheFile:   URL?
+    private var windowObserver:  Any?
+    
+    public init()
+    {
+        super.init( window: nil )
+    }
+    
+    required init?( coder: NSCoder )
+    {
+        nil
+    }
+    
+    public override var windowNibName: NSNib.Name?
+    {
+        "MainWindowController"
+    }
+    
+    public override func windowDidLoad()
+    {
+        super.windowDidLoad()
+        
+        self.configureTextView( self.configTextView )
+        self.configureTextView( self.codeTextView )
+        
+        if let cacheDirectory = NSSearchPathForDirectoriesInDomains( .cachesDirectory, .userDomainMask, true ).first,
+           let bundleID       = Bundle.main.bundleIdentifier
+        {
+            let cacheDirectory   = URL( fileURLWithPath: cacheDirectory ).appendingPathComponent( bundleID )
+            self.cacheDirectory  = cacheDirectory
+            self.configCacheFile = self.cacheDirectory?.appendingPathComponent( "config.cfg" )
+            self.codeCacheFile   = self.cacheDirectory?.appendingPathComponent( "code.txt" )
+            
+            try? FileManager.default.createDirectory( at: cacheDirectory, withIntermediateDirectories: true )
+        }
+        
+        self.restoreState()
+        
+        self.windowObserver = NotificationCenter.default.addObserver( forName: NSWindow.willCloseNotification, object: self.window, queue: nil )
+        {
+            [ weak self ] _ in self?.saveState()
+        }
+    }
+    
+    @IBAction private func loadConfig( _ sender: Any? )
+    {
+        self.chooseFile()
+        {
+            do
+            {
+                if let url = $0
+                {
+                    self.config = try self.readFile( url: url )
+                }
+            }
+            catch let error
+            {
+                self.displayError( error )
+            }
+        }
+    }
+    
+    @IBAction private func exportConfig( _ sender: Any? )
+    {
+        guard let data = self.config.data( using: .utf8 )
+        else
+        {
+            self.displayError( message: "Cannot export configuration as UTF-8 text." )
+            
+            return
+        }
+        
+        self.saveFile( data: data, name: "config", extension: "cfg" )
+    }
+    
+    @IBAction private func loadCode( _ sender: Any? )
+    {
+        self.chooseFile()
+        {
+            do
+            {
+                if let url = $0
+                {
+                    self.code = try self.readFile( url: url )
+                }
+            }
+            catch let error
+            {
+                self.displayError( error )
+            }
+        }
+    }
+    
+    @IBAction private func format( _ sender: Any? )
+    {
+        
+    }
+    
+    private func configureTextView( _ view: NSTextView )
+    {
+        view.font               = NSFont.monospacedSystemFont( ofSize: 12, weight: .regular )
+        view.textContainerInset = NSSize( width: 10, height: 10 )
+    }
+    
+    private func readFile( url: URL ) throws -> String
+    {
+        let data = try Data( contentsOf: url )
+        
+        guard let text = String( data: data, encoding: .utf8 )
+        else
+        {
+            throw NSError( title: "Cannot Read File", message: "The file \( url.lastPathComponent ) cannot be read as UTF-8 text." )
+        }
+        
+        return text
+    }
+    
+    private func saveState()
+    {
+        if let url = self.configCacheFile, let data = self.config.data( using: .utf8 )
+        {
+            try? data.write( to: url )
+        }
+        
+        if let url = self.codeCacheFile, let data = self.code.data( using: .utf8 )
+        {
+            try? data.write( to: url )
+        }
+    }
+    
+    private func restoreState()
+    {
+        if let url = self.configCacheFile, let text = try? self.readFile( url: url )
+        {
+            self.config = text
+        }
+        
+        if let url = self.codeCacheFile, let text = try? self.readFile( url: url )
+        {
+            self.code = text
+        }
+    }
+}
---
+++ Uncrustifier/Classes/NSWindowController.swift
@@ -0,0 +1,132 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Jean-David Gadina - www.xs-labs.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+import Cocoa
+import UniformTypeIdentifiers
+
+public extension NSWindowController
+{
+    func chooseFile( completion: @escaping ( URL? ) -> Void )
+    {
+        let panel                           = NSOpenPanel()
+        panel.canChooseFiles                = true
+        panel.canChooseDirectories          = false
+        panel.canDownloadUbiquitousContents = true
+        panel.canCreateDirectories          = false
+        
+        self.displayPanel( panel )
+        {
+            completion( $0 == .OK ? panel.url : nil )
+        }
+    }
+    
+    func saveFile( data: Data, name: String?, extension ext: String? )
+    {
+        let panel                  = NSSavePanel()
+        panel.canCreateDirectories = true
+        
+        if let ext = ext, let type = UTType( filenameExtension: ext )
+        {
+            panel.nameFieldStringValue = name ?? ""
+            panel.allowedContentTypes  = [ type ]
+            panel.allowsOtherFileTypes = false
+        }
+        else
+        {
+            if let name = name, let ext = ext
+            {
+                panel.nameFieldStringValue = "\( name ).\( ext )"
+            }
+            else if let name = name
+            {
+                panel.nameFieldStringValue = name
+            }
+            
+            panel.allowsOtherFileTypes = true
+        }
+        
+        self.displayPanel( panel )
+        {
+            guard $0 == .OK, let url = panel.url
+            else
+            {
+                return
+            }
+            
+            do
+            {
+                try data.write( to: url )
+            }
+            catch let error
+            {
+                self.displayError( error )
+            }
+        }
+    }
+    func displayPanel( _ panel: NSSavePanel, completion: @escaping ( NSApplication.ModalResponse ) -> Void )
+    {
+        if let window = self.window
+        {
+            panel.beginSheetModal( for: window, completionHandler: completion )
+        }
+        else
+        {
+            completion( panel.runModal() )
+        }
+    }
+    
+    func displayError( message: String )
+    {
+        self.displayError( title: "Error", message: message )
+    }
+    
+    func displayError( title: String, message: String )
+    {
+        let alert             = NSAlert()
+        alert.messageText     = title
+        alert.informativeText = message
+        
+        alert.addButton( withTitle: "OK" )
+        
+        self.displayAlert( alert )
+    }
+    
+    func displayError( _ error: Error )
+    {
+        self.displayAlert( NSAlert( error: error ) )
+    }
+    
+    func displayAlert( _ alert: NSAlert )
+    {
+        
+        if let window = self.window
+        {
+            alert.beginSheetModal( for: window )
+        }
+        else
+        {
+            alert.runModal()
+        }
+    }
+}
